cc1bb6e88fe96cd5c03e562efed904895928aef5
lewisk1899 <lewisk1899@email.arizona.edu>
2021-11-11 16:48:16 -0700

    Section 2.1 Finished Section 2.2 First More Graphs Finished

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import numpy as np
 class AS:
     def __init__(self, number):
         self.number = number
-        self.p2p = 0 # peer degree
-        self.p2c = 0 # customer degree
-        self.prov = 0 # provider degree
-        self.glob = 0 # global degree
+        self.p2p = 0  # peer degree
+        self.p2c = 0  # customer degree
+        self.prov = 0  # provider degree
+        self.glob = 0  # global degree
         self.customers = []
 
 
@@ -39,13 +39,13 @@ def parse_file(file_name):
     return as_types_count, as_counter  # return for processing data
 
 
-def pie_chart(amount):
+def pie_chart(amount, title):
     labels = "transit/access", "content", "enterprise"
     explode = (0, 0, 0)
     figure1, ax1 = plt.subplots()  # creating the figure
     ax1.pie(amount, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    plt.title('Percentage Distribution of Autonomous System Classes in 2021')
+    plt.title(title)  # Percentage Distribution of Autonomous System Classes in 2021
     plt.show()  # showing the figure
 
 
@@ -67,7 +67,7 @@ def as_classification():
     # printing findings and using them to create our needed graphs via matplotlib
     print("April 2021 as classes [transit/access, content, enterprise]:", as_types_2021, "total AS count",
           as_count_2021)
-    pie_chart(as_types_2021)
+    pie_chart(as_types_2021, "Percentage Distribution of Autonomous System Classes in 2021")
     print("August 2015 as classes [transit/access, content, enterprise]:", as_types_2015, "total AS count",
           as_count_2015)
     bar_graph(as_types_2015, as_count_2015)
@@ -76,19 +76,25 @@ def as_classification():
 #############################################################################
 # Section 2.2 Topology Inference Through AS Links
 
-def histogram(x, title):
-    arr1 = []
+def histogram(x, title, type):
     arr2 = []
-    bins_list = [0, 1, 2, 5, 101, 201, 1000]
+    bins_list = [0, 1, 2, 5, 101, 201, 1000]  # bins
     for as_i in x:
-        arr1.append(int(as_i.number))
-        arr2.append(as_i.glob)
-    hist, bin_edges = np.histogram(arr2, bins=bins_list)
+        if type == 'Global':
+            arr2.append(as_i.glob)
+        elif type == 'Customer':
+            arr2.append(as_i.p2c)
+        elif type == 'Peer':
+            arr2.append(as_i.p2p)
+        elif type == 'Provider':
+            arr2.append(as_i.prov)
+    hist, bin_edges = np.histogram(arr2, bins=bins_list)  # histogram
     fig, ax = plt.subplots()
     ax.bar(range(len(hist)), hist, width=1, edgecolor='k')
-    ax.set_xlabel(title)
-    ax.set_ylabel('Number of Autonomous Systems')
-    ax.set_title(r'Number of Autonomous Systems that hold x', title)
+    ax.set_xlabel(title)  # x-axis title
+    ax.set_ylabel('Number of Autonomous Systems')  # y-axis title
+    title = 'Number of Autonomous Systems that hold ' + title
+    ax.set_title(title)  # title
     ax.set_xticks(range(len(bins_list) - 1))
     ax.set_xticklabels(['{} - {}'.format(bins_list[i], bins_list[i + 1]) for i, j in enumerate(hist)])
     plt.show()
@@ -102,8 +108,9 @@ def inc_degree(as_1, as_2, classification):
     # it is just a customer to provider connection
     else:
         as_1.prov += 1
-        as_1.customers.append(as_2) # as1 is the provider to as2, therefore we will add as2 to as1s customer list
+        as_1.customers.append(as_2)  # as1 is the provider to as2, therefore we will add as2 to as1s customer list
         as_2.p2c += 1
+
     as_1.glob += 1
     as_2.glob += 1
 
@@ -140,26 +147,41 @@ def section_2b(filename):
                     if AutoSys2.number == split_line[1]:
                         temp_as2 = AutoSys2
 
-            inc_degree(temp_as1, temp_as2, split_line[2]) # function to handle altering respective private variables
+            inc_degree(temp_as1, temp_as2, split_line[2])  # function to handle altering respective private variables
 
         i += 1
+        if i > 1000:
+            break
+
         print(i)
 
     return list_as_objects
 
 
+def piechart_2(_as_list,title):
+    data = [0, 0, 0]  # Enterprise, Content, Transit
+    for _as in _as_list:
+        if _as.p2p == 0 and _as.p2c == 0:
+            data[0] += 1
+        if _as.p2p >= 1 and _as.p2c == 0:
+            data[1] += 1
+        if _as.p2c >= 1:
+            data[2] += 1
+    pie_chart(data, title)
+
+
 #################################################################################################################################
 
-def main():
+def run():
+    as_classification()
     # test
     list_obj = section_2b("20211001.as-rel2.txt")
     # list_obj = section_2b("testfor2b.txt")
-    # histogram(list_obj, "Global Node Degree")
-    histogram(list_obj, "Customer Degree")
-    # histogram(list_obj, "Peer Degree")
-    # histogram(list_obj, "Provider Degree")
-    # pie_chart(list_obj, "Class Distribution of of ASes")
+    histogram(list_obj, "Global Node Degree", 'Global')
+    histogram(list_obj, "Customer Degree", 'Customer')
+    histogram(list_obj, "Peer Degree", 'Peer')
+    histogram(list_obj, "Provider Degree", 'Provider')
 
+    piechart_2(list_obj, 'Percentage Distribution of Autonomous System Classes in 2021 According to Link Traversal')
 
-if __name__ == "__main__":
-    main()
+run()
